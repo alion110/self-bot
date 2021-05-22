e2729c85523a9df9516cdf981beeb1fb64e94660
Alion <mymrali12@gmail.com>
2021-05-23 02:28:01 +0430

    Update j.py

--- j.py
+++ j.py
@@ -1,6 +1,9 @@
 #Alion Self-Bot
 from pyrogram import Client
 print('>>> BOT STARTED')
-Client("bot",api_id = 4109627,api_hash = '5874fe255bec03272faaa41ac22e55dc',config_file='config.ini').run()
+try:
+  Client("bot",api_id = 4109627,api_hash = '5874fe255bec03272faaa41ac22e55dc',config_file='config.ini').run()
+except Exception as r:
+  print(r)
 print('\n>>> BOT STOPPED')
 
