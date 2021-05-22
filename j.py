f1a27546f5571b38f82966c911a6db571ca06503
Alion <mymrali12@gmail.com>
2021-05-23 02:13:56 +0430

    Add files via upload

---
+++ j.py
@@ -0,0 +1,6 @@
+#Alion Self-Bot
+from pyrogram import Client
+print('>>> BOT STARTED')
+Client("bot",api_id = 4109627,api_hash = '5874fe255bec03272faaa41ac22e55dc',config_file='config.ini').run()
+print('\n>>> BOT STOPPED')
+
