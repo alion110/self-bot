012d47f5447bfd91d76849b77f1ba3d563de2cbd
Alion <mymrali12@gmail.com>
2021-05-24 21:14:02 +0430

    Update fid.py

--- plugins/fid.py
+++ plugins/fid.py
@@ -17,6 +17,14 @@ async def fid(client, message):
     rp = message.reply_to_message
     print(rp)
     await client.send_message(message.chat.id, rp, reply_to_message_id=rp.message_id)
+    
+@app.on_message(filters.me & filters.command(['send'],['/','!','+','-','','*','~','#','$','']))
+async def send_by_file_id(client, message):
+    what = 'sticker' #message.text.split('-')[1].split(' ')[0]
+    send_id = message.text.split(' ')[1]
+    print(what, send_id)
+    if what == 'sticker':
+        await client.send_sticker(message.chat.id, send_id)
 
 
 
