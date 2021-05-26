c7a3f1eda59079a36101595510a4d10cbc653af6
Alion <mymrali12@gmail.com>
2021-05-26 19:22:39 +0430

    Update autoblock.py

--- plugins/autoblock.py
+++ plugins/autoblock.py
@@ -1,16 +1,18 @@
 from pyrogram import Client as app, filters
 import json
-with open('users.json', 'r', encoding='utf-8') as jsonfile:
-    data = json.load(jsonfile)
-if data['autoblock'] == 1:
-    autoblock = True
-else:
-    autoblock = False
+
+
 
 @app.on_message(filters.me & filters.command(['ablock'], ['']))
 async def autoblock_en(client, message):
     global autoblock
-    if autoblock:
+    with open('users.json', 'r', encoding='utf-8') as jsonfile:
+        data = json.load(jsonfile)
+    if data['autoblock'] == 1:
+        autoblock = True
+    else:
+        autoblock = False
+    if autoblock == True:
         data['autoblock'] = 2
         with open('users.json', 'w', encoding='utf8') as jsonfile:
             json.dump(data, jsonfile, indent=4)
@@ -25,7 +27,7 @@ async def privater323(client, message):
     if message.from_user.id == 1223702732:
         return
     if autoblock == True:
-        await client.send_message(message.chat.id,'در راستای جلوگیری از اسپم توسط افراد خاص اکانت شما به صورت موقت بلاک شد')
+        #await client.send_message(message.chat.id,'در راستای جلوگیری از اسپم توسط افراد خاص اکانت شما به صورت موقت بلاک شد')
         await client.block_user(message.from_user.id)
     elif autoblock == False:
         return
