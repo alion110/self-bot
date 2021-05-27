1fcc8291c3d9a4b5dc5e78f4ef230bf5d3f4f31d
Alion <mymrali12@gmail.com>
2021-05-28 02:56:03 +0430

    Update bg.py

--- plugins/bg.py
+++ plugins/bg.py
@@ -6,24 +6,18 @@ async def remove_bg(client, message):
     if 1 == 1:
         rp = message.reply_to_message
         if rp.photo or rp.sticker:
-            await client.edit_message_text(message.chat.id, message.message_id, '**Downloading .**')
-            await client.edit_message_text(message.chat.id, message.message_id, '**Downloading ..**')
             await client.edit_message_text(message.chat.id, message.message_id, '**Downloading ...**')
             await rp.download('bg.jpg')
-            await client.edit_message_text(message.chat.id, message.message_id, '**Processing .**')
-            await client.edit_message_text(message.chat.id, message.message_id, '**Processing ..**')
             await client.edit_message_text(message.chat.id, message.message_id, '**Processing ...**')
             response = requests.post(
                 'https://api.remove.bg/v1.0/removebg',
                 files={'image_file': open('downloads/bg.jpg', 'rb')},
                 data={'size': 'auto'},
-                headers={'X-Api-Key': 'nRG1x3QJwR3ghABkeJWiyiia'},
+                headers={'X-Api-Key': 'w2Jzds6HyXiRCitzdghZRDdA'},
             )
             if response.status_code == requests.codes.ok:
                 with open('no-bg.png', 'wb') as out:
                     out.write(response.content)
-                await client.edit_message_text(message.chat.id, message.message_id, '**Uploading .**')
-                await client.edit_message_text(message.chat.id, message.message_id, '**Uploading ..**')
                 await client.edit_message_text(message.chat.id, message.message_id, '**Uploading ...**')
                 await client.send_photo(message.chat.id, 'no-bg.png', caption='Background Successfully Removed :)', reply_to_message_id=rp.message_id)
                 await client.send_document(message.chat.id, 'no-bg.png', caption='Background Successfully Removed :)', reply_to_message_id=rp.message_id)
