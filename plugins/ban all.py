76330799a412932af8d55ec91a7d9241e2c8ffbc
Alion <mymrali12@gmail.com>
2021-05-28 00:22:04 +0430

    Update ban all.py

--- plugins/ban all.py
+++ plugins/ban all.py
@@ -5,9 +5,9 @@ import asyncio
 def clmembers(client, message):
     try:
         limit = message.text.split(' ')[1] if int(message.text.split(' ')[1]) else 500
-        await client.send_message(message.chat.id, 'loading members ...')
+        client.send_message(message.chat.id, 'loading members ...')
         for member in await client.iter_chat_members(message.chat.id, limit=int(limit)):
-            await client.kick_chat_member(message.chat.id, member.user.id)
-        await client.send_message(message.chat.id, 'انجام شد')
+            client.kick_chat_member(message.chat.id, member.user.id)
+        client.send_message(message.chat.id, 'انجام شد')
     except FloodWait as r:
         asyncio.sleep(r.x)
