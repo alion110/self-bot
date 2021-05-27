b824701d25a3e1c8ed898396bc0f240346351995
Alion <mymrali12@gmail.com>
2021-05-28 00:18:56 +0430

    Add files via upload

---
+++ plugins/ban all.py
@@ -0,0 +1,13 @@
+from pyrogram import Client as app , filters
+from pyrogram.errors import FloodWait
+import asyncio
+@app.on_message(filters.me & filters.command(['clm'], ['/','!','+','-','']))
+async def clmembers(client, message):
+    try:
+        limit = message.text.split(' ')[1] if int(message.text.split(' ')[1]) else 500
+        await client.send_message(message.chat.id, 'loading members ...')
+        for member in client.iter_chat_members(message.chat.id, limit=limit):
+            await client.kick_chat_member(message.chat.id, member.user.id)
+        await client.send_message(message.chat.id, 'انجام شد')
+    except FloodWait as r:
+        asyncio.sleep(r.x)
