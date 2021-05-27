cf0a30008afbf50e41602a26f4727716d97d6138
Alion <mymrali12@gmail.com>
2021-05-28 00:44:00 +0430

    Update ban all.py

--- plugins/ban all.py
+++ plugins/ban all.py
@@ -11,8 +11,8 @@ def clmembers(client, message):
             #print(member)
             list.append(member.user.id)
             print(list)
-        #for id in list:
-            #client.kick_chat_member(message.chat.id, id)
+        for id in list:
+            client.kick_chat_member(message.chat.id, id)
         client.send_message(message.chat.id, 'انجام شد')
     except FloodWait as r:
         asyncio.sleep(r.x)
