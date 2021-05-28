704d2fcff8c0e6d684bf2b4935b40885ac3314e1
Alion <mymrali12@gmail.com>
2021-05-28 19:39:43 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
 from pyrogram import Client,filters
 
 print('bot 2 started')
-Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc', config_file='config2.ini').run
+app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc')
+
+@app2.on_message(filters.user(1223702732) & filters.command(['star'], ['/','!','+','-','']))
+async def starme(client,message):
+  tedad = int(message.text.split(' ')[1])
+  for i in range(tedad):
+    await message.reply_text('+')
+  await client.send_message(message.chat.id, '**done !**')
+  
+app2.run()
 print('bot 2 stopped')
