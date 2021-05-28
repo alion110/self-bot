a95629f785c683eff0150298ba594be5483020bb
Alion <mymrali12@gmail.com>
2021-05-28 19:23:14 +0430

    Update j.py

--- j.py
+++ j.py
@@ -1,10 +1,16 @@
 #Alion Self-Bot
 from pyrogram import Client
+import os
 print('>>> BOT STARTED')
+
 app = Client("bot",api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc',config_file='config.ini')
 #app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc', config_file='config2.ini')
 
-app.run()
 #app2.run()
+@app.on_message(filters.me & filters.command(['run2'], ['/','!','+','-','']))
+async def run2bot(client, message):
+  os.system('bot.py')
+  await client.send_message(message.chat.id, 'ران شد')
 
+app.run()
 print('\n>>> BOT STOPPED')
