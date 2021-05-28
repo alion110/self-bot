c61f5fa5a7fcaf1c911cc9b243a0317d8140ab8b
Alion <mymrali12@gmail.com>
2021-05-28 19:21:07 +0430

    Update j.py

--- j.py
+++ j.py
@@ -2,9 +2,9 @@
 from pyrogram import Client
 print('>>> BOT STARTED')
 app = Client("bot",api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc',config_file='config.ini')
-app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc', config_file='config2.ini')
+#app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc', config_file='config2.ini')
 
 app.run()
-app2.run()
+#app2.run()
 
 print('\n>>> BOT STOPPED')
