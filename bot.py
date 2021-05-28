a6541edd7d7e235f94133f2c903cf0113d43ca4e
Alion <mymrali12@gmail.com>
2021-05-28 19:24:29 +0430

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,4 @@
+from pyrogram import Client
+print('bot 2 started')
+Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc', config_file='config2.ini').run
+print('bot 2 stopped')
