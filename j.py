1479df2adce72e19f519103911e25611e5350303
Alion <mymrali12@gmail.com>
2021-05-28 19:49:54 +0430

    Update j.py

--- j.py
+++ j.py
@@ -4,13 +4,12 @@ import os
 print('>>> BOT STARTED')
 
 app = Client("bot",api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc',config_file='config.ini')
-#app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc', config_file='config2.ini')
+app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc')
 
 #app2.run()
 @app.on_message(filters.me & filters.command(['run2'], ['/','!','+','-','']))
 async def run2bot(client, message):
-  os.system('python3 bot.py')
   await client.send_message(message.chat.id, 'ران شد')
-
+  app2.run()
 app.run()
 print('\n>>> BOT STOPPED')
