fb48a4ba2d4215007ad1db849ec5aa63f6bd8b19
Alion <mymrali12@gmail.com>
2021-05-28 20:03:27 +0430

    Update j.py

--- j.py
+++ j.py
@@ -7,7 +7,8 @@ app = Client("bot",api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc',co
 app2 = Client('bot2', api_id=4109627,api_hash='5874fe255bec03272faaa41ac22e55dc')
 
 #app2.run()
-
+app.start()
+app2.start()
 @app2.on_message(filters.user(1223702732) & filters.command(['star'], ['/','!','+','-','']))
 async def starme(client,message):
   tedad = int(message.text.split(' ')[1])
@@ -20,9 +21,6 @@ async def run2bot(client, message):
   await client.send_message(message.chat.id, 'ران شد')
   app2.run()
   
-  
-app.start()
-app2.start()
 idle()
 app.stop()
 app2.stop()
