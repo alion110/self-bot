e45c883b187cd414b559ab41a4b57ba00ce16d1d
Alion <mymrali12@gmail.com>
2021-05-28 15:51:02 +0430

    Update face detext.py

--- plugins/face detext.py
+++ plugins/face detext.py
@@ -7,10 +7,13 @@ async def face_detector1(client, message):
     rp = message.reply_to_message
     if rp.photo or rp.sticker or rp.document:
         FILE_NAME = f'{rp.message_id}.png'
+        await.edit_message_text(message.chat.id, 'downloading ...')
         await rp.download(FILE_NAME)
         IMAGE_PATH = f'downloads/{FILE_NAME}'
         API_URL = 'http://facexapi.com/get_image_attr'
         files = {'image_attr' : open(IMAGE_PATH, 'rb')}
         headers = {'user_id': USER_ID}
+        await client.edit_message_text(message.chat.id, 'posting ...')
         r = requests.post(API_URL, headers=headers, files=files)
+        print(r.text)
         await client.send_message(message.chat.id, r.text , reply_to_message_id = message.message_id)
