fcc1006c1048f8af943f0e3299e9bc150fe6c63d
Alion <mymrali12@gmail.com>
2021-06-01 18:49:59 +0430

    Update kir.py

--- plugins/kir.py
+++ plugins/kir.py
@@ -3,14 +3,15 @@ from pyrogram import filters
 import asyncio
 
 
-@app.on_message(filters.me & filters.command(['kir'], ['/', '!', '+', '-', ''])
-                )
+@app.on_message(filters.me & filters.command(['kir'], ['/', '!', '+', '-', '']))
 async def kirekhar(client, message):
+    await client.delete_messages(message.chat.id, message.message_id)
+    text = message.text.split('kir')[1].split('=')[0]
+    tedad = int(message.text.split('=')[1])
     if message.reply_to_message:
         rp = message.reply_to_message.message
+        await asyncio.gether(*[client.send_message(message.chat.id, text for i in range(tedad), reply_to_message_id=rp)])
     elif not message.reply_to_message:
-        rp = None
-    text = message.text.split('kir')[1].split('=')[0]
-    tedad = message.text.split('=')[1]
-    await client.delete_messages(message.chat.id, message.message_id)
-    await asyncio.gather(*[client.send_message(message.chat.id, str(text) for i in range(int(range)),reply_to_message_id=rp)])
+        await asyncio.gether(*[client.send_message(message.chat.id, text for i in range(tedad))])
+
+
