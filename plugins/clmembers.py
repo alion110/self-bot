7b1bbd60021ad2e22306392a9a8ac3169f9981a9
Alion <mymrali12@gmail.com>
2021-06-01 09:16:46 +0430

    Create clmembers.py

---
+++ plugins/clmembers.py
@@ -0,0 +1,17 @@
+from pyrogram import Client as app,filters 
+
+
+@app.on_message(filters.group & filters.me & filters.command(['clm'],['/','!','+','-','','*','~','#']))
+def clmember(client, message):
+    can_not_remove = ['administrator', 'creator']
+    try:
+        client.edit_message_text(message.chat.id,message.message_id ,'**Deleting ...**')
+        for member in client.iter_chat_members(message.chat.id):
+            if member.status in can_not_remove:
+                return
+            client.kick_chat_member(message.chat.id, member.user.id)
+        client.delete_messages(message.chat.id, message.message_id)
+        client.send_message(message.chat.id, 'Done :)')
+
+    except Exception as r:
+        client.send_message(message.chat.id, f'```~ERROR : ``` {r}')
