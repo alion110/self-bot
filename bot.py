07a2c6ffc952fd171a82bcded6317eac5a36c245
Alion <mymrali12@gmail.com>
2021-06-01 19:24:18 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from pyrogram import Client,filters
+from pyrogram import Client,filters,idle
 import configparser
 configs = configparser.ConfigParser()
 configs.read("config.ini")
@@ -7,8 +7,12 @@ api_hash = str(configs.get("pyrogram","api_hash"))
 print('>>> BOT STARTED')
 
 app = Client("bot",api_id,api_hash,config_file='config.ini')
+bot = Client("bot",api_id,api_hash,bot_token='1899785392:AAFvsm7835MbYcvKl0BkHTYuuOvhGa8HGc0',config_file='config.ini')
+app.start()
+bot.start()
 
-app.run()
+bot.stop()
+app.stop()
 
 print('\n>>> BOT STOPPED')
 
