ce09d3e19c8ccfad261bba762b9ec9d3b34d4d03
Alion <mymrali12@gmail.com>
2021-06-01 18:16:09 +0430

    Update kir.py

--- plugins/kir.py
+++ plugins/kir.py
@@ -3,15 +3,14 @@ from pyrogram import filters
 import asyncio
 
 
-@app.on_message(filters.me & filters.command(['kir'], ['/', '!', '+', '-', '']))
+@app.on_message(filters.me & filters.command(['kir'], ['/', '!', '+', '-', ''])
+                )
 async def kirekhar(client, message):
     if message.reply_to_message:
         rp = message.reply_to_message.message
     elif not message.reply_to_message:
         rp = None
+    text = message.text.split('kir')[1].split('=')[0]
+    tedad = message.text.split('=')[1]
     await client.delete_messages(message.chat.id, message.message_id)
-    await asyncio.gather(*[client.send_messages(message.chat.id, message.text.split('kir')[1].split('=')[0] for i in range(int(message.text.split('=')[1]),reply_to_message_id=rp))])
-
-
-
-
+    await asyncio.gather(*[client.send_message(message.chat.id, str(text) for i in range(int(range)),reply_to_message_id=rp)])
