17bed21cd46eada558478e70ef800820e00ace88
Alion <mymrali12@gmail.com>
2021-06-01 17:58:12 +0430

    Update kir.py

--- plugins/kir.py
+++ plugins/kir.py
@@ -8,7 +8,7 @@ async def kirekhar(client, message):
     elif not message.reply_to_message:
         rp = None
     await client.delete_messages(message.chat.id, message.message_id)
-    await asyncio.gather(*[client.send_message(message.chat.id, message.text.split('kir')[1].split('=')[0] for i in range(int(message.text.split('=')[1])), reply_to_message_id=rp)
+    await asyncio.gather(*[client.send_message(message.chat.id, message.text.split('kir')[1].split('=')[0] for i in range(int(message.text.split('=')[1])), reply_to_message_id=rp])
 
 
 
