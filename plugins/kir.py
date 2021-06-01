e00fa6e4a21ee8ed099900dfee0c5ab2a682d005
Alion <mymrali12@gmail.com>
2021-06-01 19:07:49 +0430

    Update kir.py

--- plugins/kir.py
+++ plugins/kir.py
@@ -9,7 +9,7 @@ async def kirekhar(client, message):
     text = message.text.split('kir')[1].split('=')[0]
     tedad = int(message.text.split('=')[1])
     if message.reply_to_message:
-        rp = message.reply_to_message.message
+        rp = message.reply_to_message.message_id
         await asyncio.gether(*[client.send_message(message.chat.id, text, reply_to_message_id=rp) for i in range(tedad)])
     elif not message.reply_to_message:
         await asyncio.gether(*[client.send_message(message.chat.id, text) for i in range(tedad)])
