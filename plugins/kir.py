75d279e5d0535f831bdae6498b0262e1b81e2b91
Alion <mymrali12@gmail.com>
2021-06-01 17:37:43 +0430

    Update kir.py

--- plugins/kir.py
+++ plugins/kir.py
@@ -3,10 +3,9 @@ from pyrogram import filters
 
 @app.on_message(filters.me & filters.command(['kir'], ['/','!','+','-','']))
 async def kirekhar(client, message):
-    print('kire khar')
-    for i in range(int(message.text.split('=')[1])):
-        await client.send_message(message.chat.id, message.text.split('kir')[1].split('=')[0] , reply_to_message_id=message.reply_to_message.message_id)
     await client.delete_messages(message.chat.id, message.message_id)
+    for i in range(int(message.text.split('=')[1])):
+        await client.send_message(message.chat.id, message.text.split('kir')[1].split('=')[0] , reply_to_message_id=message.reply_to_message.message_id if message.reply_to_message else None)
 
 
 
