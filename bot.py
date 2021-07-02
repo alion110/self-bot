7dc3eec2713a0ba45ed86245e9ff5f46915b2410
Alion <mymrali12@gmail.com>
2021-07-02 22:27:58 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,6 @@ print('>>> BOT STARTED')
 
 app = Client('1BJWap1sBu0let_yfAfQYseESxLaUncqOE89aWOpc3IrMXTCD7F83rKi4aYIAGEdTBiqL26GU6Wa1n-kOKEVUGQ8wq8_4R72GV--EuBVlCh0zu7uH1WnR8rtQIy4guH-6Q_dn_gC9OKI2DT78jTWVCArO4pRoYfNO4jk8w1-j3W5pjuQa3nQi3UMERC3vA9_bAQ1sTbs0nSWyCnPnCew3YpFwLeLzLQMhbPcVf_zQbEuUQyKKlgGbie8NE7r8aCJkKrPBqWGIkw0nozT_HIZOrvo75DCfq6sEwZ9Pc3BUrhd-m9vUu1326g3brw9p_aSCoYG1puu98hgReChK9mosgxApOeTluDU=',config_file='config.ini')
 
-app.run
+app.run()
 print('\n>>> BOT STOPPED')
 
