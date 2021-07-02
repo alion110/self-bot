41bb833a20323aa6d116c6065a41c225c39aabce
Alion <mymrali12@gmail.com>
2021-07-02 23:14:00 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ api_id = int(configs.get("pyrogram","api_id"))
 api_hash = str(configs.get("pyrogram","api_hash"))
 print('>>> BOT STARTED')
 
-app = Client('1BJWap1sBu0let_yfAfQYseESxLaUncqOE89aWOpc3IrMXTCD7F83rKi4aYIAGEdTBiqL26GU6Wa1n-kOKEVUGQ8wq8_4R72GV--EuBVlCh0zu7uH1WnR8rtQIy4guH-6Q_dn_gC9OKI2DT78jTWVCArO4pRoYfNO4jk8w1-j3W5pjuQa3nQi3UMERC3vA9_bAQ1sTbs0nSWyCnPnCew3YpFwLeLzLQMhbPcVf_zQbEuUQyKKlgGbie8NE7r8aCJkKrPBqWGIkw0nozT_HIZOrvo75DCfq6sEwZ9Pc3BUrhd-m9vUu1326g3brw9p_aSCoYG1puu98hgReChK9mosgxApOeTluDU=',config_file='config.ini')
+app = Client('1BJWap1sBu3gGrhvj2CQrEXoDHpww-H6XQb1X0XSQ8jX8ZEMz2aEn2ZGRGtVKKil7KdsmfmD1h-JVnR-68DEBLcCyXJsMN7TOIUVWyxpcCcCntKuVMhC91Crbi0UknPHfaqRyz7AwjKIqeTMjIIFPoh6SkL0-G9ppW43C7fhT5H6vWA9caZS87IstKxfy6JS7rpjSsvTH9LLFtbebR6TEvbUq98UFNwzr3YzVOzY6n24OeYrX4xXQoKdGxUVbVkg8EOLrw6eGJvRXx9Y9mBvExW0uKC4gb0I581YTjt_-3SB8oz-ixc2uC3AOBWr1fbY657zeTXM82lpmMp3unbe-1hoNgDRXwh0=',config_file='config.ini')
 
 app.run()
 print('\n>>> BOT STOPPED')
