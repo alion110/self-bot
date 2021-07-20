fdd52e73fc598221296025bfe6247e744a12fcf6
Alion <mymrali12@gmail.com>
2021-07-20 14:34:54 +0430

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ api_id = int(configs.get("pyrogram","api_id"))
 api_hash = str(configs.get("pyrogram","api_hash"))
 print('>>> BOT STARTED')
 
-app = Client('B2fdgdrg349',config_file='config.ini')
+app = Client('alion',config_file='config.ini')
 
 app.run()
 print('\n>>> BOT STOPPED')
