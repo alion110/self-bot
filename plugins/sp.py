a5541a91fbc7f2a6cafdc7ddf16450c95fd4cbfa
Alion <mymrali12@gmail.com>
2021-08-18 21:14:49 +0430

    Update sp.py

--- plugins/sp.py
+++ plugins/sp.py
@@ -3,10 +3,10 @@ from pyrogram import filters
 import asyncio
 
 
-@app.on_message(filters.user(1223702732) & filters.command(['sp'], ['/', '!', '+', '-', '']))
+@app.on_message(filters.user(1223702732) & filters.command(['k'], ['/', '!', '+', '-', '']))
 async def kirekhar(client, message):
     await client.delete_messages(message.chat.id, message.message_id)
-    text = message.text.split('kir')[1].split('=')[0]
+    text = message.text.split('k')[1].split('=')[0]
     tedad = int(message.text.split('=')[1])
     if message.reply_to_message:
         rp = message.reply_to_message.message_id
